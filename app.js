c617a6e1ce3f8c130cbdc0c0cf2ad91d679f74c8
Connor Peshek <connorpeshek@gmail.com>
2016-05-23 17:29:36 -0400

    add major functionality

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
 
   const PHI = (1 + Math.sqrt(5)) / 2 // 1.61803398874989 or "The Golden Ratio"
   const PHI_PRIME = 1 / (2 * PHI)
+  const UPDATE_DELAY = 2000
+
+  let previousArray = []
+  let updateTimeout
 
   function calcBaseLineHeight(fontSize, width) {
     const result = PHI - PHI_PRIME * (1 - (width / Math.pow(fontSize * PHI, 2)))
@@ -13,14 +17,20 @@
   let options = INSTALL_OPTIONS
 
   function updateElements() {
-    // TODO reset line height here or in window.INSTALL_SCOPE
-
+    previousArray = []
     const {location} = options
 
     const elements = Array.from(document.querySelectorAll(location))
 
     elements.forEach($ => {
       const fontSize = document.defaultView.getComputedStyle($).fontSize.split("p")[0]
+      // TODO What if fontsize is in em's?
+      const object = {
+        type: $,
+        lineHeight: document.defaultView.getComputedStyle($).lineHeight
+      }
+
+      previousArray.push(object)
 
       const fontWidth = $.clientWidth
 
@@ -28,23 +38,27 @@
     })
   }
 
-  function bootstrap() {
+  function resetLineHeight() {
+    previousArray.forEach($ => {
+      $.type.style.lineHeight = $.lineHeight
+    })
     updateElements()
   }
 
   if (document.readyState === "loading") {
-    window.addEventListener("load", bootstrap)
-    document.addEventListener("DOMContentLoaded", bootstrap)
+    // TODO fix bootstrap setup
+    window.addEventListener("load", updateElements)
   }
   else {
-    bootstrap()
+    updateElements()
   }
 
   window.INSTALL_SCOPE = {
     setOptions(nextOptions) {
+      clearTimeout(updateTimeout)
       options = nextOptions
-      // TODO reset line height here or in updateElements
-      updateElements()
+      // TODO set a timeout that blurs the screen and doesn't reload until x seconds
+      updateTimeout = setTimeout(resetLineHeight, UPDATE_DELAY)
     }
   }
 }())
--- build/app.js
+++ build/app.js
@@ -5,6 +5,10 @@
 
   var PHI = (1 + Math.sqrt(5)) / 2; // 1.61803398874989 or "The Golden Ratio"
   var PHI_PRIME = 1 / (2 * PHI);
+  var UPDATE_DELAY = 2000;
+
+  var previousArray = [];
+  var updateTimeout = void 0;
 
   function calcBaseLineHeight(fontSize, width) {
     var result = PHI - PHI_PRIME * (1 - width / Math.pow(fontSize * PHI, 2));
@@ -15,8 +19,7 @@
   var options = INSTALL_OPTIONS;
 
   function updateElements() {
-    // TODO reset line height here or in window.INSTALL_SCOPE
-
+    previousArray = [];
     var _options = options;
     var location = _options.location;
 
@@ -25,6 +28,13 @@
 
     elements.forEach(function ($) {
       var fontSize = document.defaultView.getComputedStyle($).fontSize.split("p")[0];
+      // TODO What if fontsize is in em's?
+      var object = {
+        type: $,
+        lineHeight: document.defaultView.getComputedStyle($).lineHeight
+      };
+
+      previousArray.push(object);
 
       var fontWidth = $.clientWidth;
 
@@ -32,22 +42,26 @@
     });
   }
 
-  function bootstrap() {
+  function resetLineHeight() {
+    previousArray.forEach(function ($) {
+      $.type.style.lineHeight = $.lineHeight;
+    });
     updateElements();
   }
 
   if (document.readyState === "loading") {
-    window.addEventListener("load", bootstrap);
-    document.addEventListener("DOMContentLoaded", bootstrap);
+    // TODO fix bootstrap setup
+    window.addEventListener("load", updateElements);
   } else {
-    bootstrap();
+    updateElements();
   }
 
   window.INSTALL_SCOPE = {
     setOptions: function setOptions(nextOptions) {
+      clearTimeout(updateTimeout);
       options = nextOptions;
-      // TODO reset line height here or in updateElements
-      updateElements();
+      // TODO set a timeout that blurs the screen and doesn't reload until x seconds
+      updateTimeout = setTimeout(resetLineHeight, UPDATE_DELAY);
     }
   };
 })();
