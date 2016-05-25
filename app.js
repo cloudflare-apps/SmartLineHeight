f898d0e11822eb7e20eeb62b0e1d817e83389a78
Connor Peshek <connorpeshek@gmail.com>
2016-05-25 19:49:34 -0400

    Wrap up functionality.
    - Reduce render jitteryness
    - Simplify options

--- app.js
+++ app.js
@@ -3,62 +3,59 @@
 
   const PHI = (1 + Math.sqrt(5)) / 2 // 1.61803398874989 or "The Golden Ratio"
   const PHI_PRIME = 1 / (2 * PHI)
-  const UPDATE_DELAY = 2000
 
-  let previousArray = []
-  let updateTimeout
+  let previousElements = []
+  let options = INSTALL_OPTIONS
 
   function calcBaseLineHeight(fontSize, width) {
-    const result = PHI - PHI_PRIME * (1 - (width / Math.pow(fontSize * PHI, 2)))
+    const result = PHI - PHI_PRIME * (1 - width / Math.pow(fontSize * PHI, 2))
 
     return Math.round(result * fontSize) / fontSize // Lower precision.
   }
 
-  let options = INSTALL_OPTIONS
-
   function updateElements() {
-    previousArray = []
-    const {location} = options
+    const elements = options.selectors
+      .map(entry => document.querySelectorAll(entry.selector))
+      .reduce((elements, subElements) => {
+        elements.push(...subElements)
 
-    const elements = Array.from(document.querySelectorAll(location))
+        return elements
+      }, [])
 
-    elements.forEach($ => {
-      const fontSize = document.defaultView.getComputedStyle($).fontSize.split("p")[0]
-      // TODO What if fontsize is in em's?
-      const object = {
-        type: $,
-        lineHeight: document.defaultView.getComputedStyle($).lineHeight
+    elements.forEach(element => {
+      const fontSize = parseFloat(document.defaultView.getComputedStyle(element).fontSize)
+
+      if (INSTALL_ID === "preview") {
+        previousElements.push({element, lineHeight: element.style.lineHeight})
       }
 
-      previousArray.push(object)
-
-      const fontWidth = $.clientWidth
-
-      $.style.lineHeight = calcBaseLineHeight(fontSize, fontWidth)
+      element.style.lineHeight = calcBaseLineHeight(fontSize, element.clientWidth)
     })
+
+    document.body.setAttribute("data-eager-readable-state", "loaded")
   }
 
-  function resetLineHeight() {
-    previousArray.forEach($ => {
-      $.type.style.lineHeight = $.lineHeight
-    })
-    updateElements()
+  function bootstrap() {
+    document.body.setAttribute("data-eager-readable-state", "loading")
   }
 
   if (document.readyState === "loading") {
-    // TODO fix bootstrap setup
     window.addEventListener("load", updateElements)
+    document.addEventListener("DOMContentLoaded", bootstrap)
   }
   else {
+    bootstrap()
     updateElements()
   }
 
   window.INSTALL_SCOPE = {
     setOptions(nextOptions) {
-      clearTimeout(updateTimeout)
       options = nextOptions
-      // TODO set a timeout that blurs the screen and doesn't reload until x seconds
-      updateTimeout = setTimeout(resetLineHeight, UPDATE_DELAY)
+
+      previousElements.forEach(({element, lineHeight}) => element.style.lineHeight = lineHeight)
+      previousElements = []
+
+      updateElements()
     }
   }
 }())
--- build/app.js
+++ build/app.js
@@ -1,14 +1,15 @@
 "use strict";
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 (function () {
   if (!window.addEventListener) return; // Check for IE9+
 
   var PHI = (1 + Math.sqrt(5)) / 2; // 1.61803398874989 or "The Golden Ratio"
   var PHI_PRIME = 1 / (2 * PHI);
-  var UPDATE_DELAY = 2000;
 
-  var previousArray = [];
-  var updateTimeout = void 0;
+  var previousElements = [];
+  var options = INSTALL_OPTIONS;
 
   function calcBaseLineHeight(fontSize, width) {
     var result = PHI - PHI_PRIME * (1 - width / Math.pow(fontSize * PHI, 2));
@@ -16,52 +17,52 @@
     return Math.round(result * fontSize) / fontSize; // Lower precision.
   }
 
-  var options = INSTALL_OPTIONS;
-
   function updateElements() {
-    previousArray = [];
-    var _options = options;
-    var location = _options.location;
+    var elements = options.selectors.map(function (entry) {
+      return document.querySelectorAll(entry.selector);
+    }).reduce(function (elements, subElements) {
+      elements.push.apply(elements, _toConsumableArray(subElements));
 
+      return elements;
+    }, []);
 
-    var elements = Array.from(document.querySelectorAll(location));
+    elements.forEach(function (element) {
+      var fontSize = parseFloat(document.defaultView.getComputedStyle(element).fontSize);
 
-    elements.forEach(function ($) {
-      var fontSize = document.defaultView.getComputedStyle($).fontSize.split("p")[0];
-      // TODO What if fontsize is in em's?
-      var object = {
-        type: $,
-        lineHeight: document.defaultView.getComputedStyle($).lineHeight
-      };
+      if (INSTALL_ID === "preview") {
+        previousElements.push({ element: element, lineHeight: element.style.lineHeight });
+      }
 
-      previousArray.push(object);
-
-      var fontWidth = $.clientWidth;
-
-      $.style.lineHeight = calcBaseLineHeight(fontSize, fontWidth);
+      element.style.lineHeight = calcBaseLineHeight(fontSize, element.clientWidth);
     });
+
+    document.body.setAttribute("data-eager-readable-state", "loaded");
   }
 
-  function resetLineHeight() {
-    previousArray.forEach(function ($) {
-      $.type.style.lineHeight = $.lineHeight;
-    });
-    updateElements();
+  function bootstrap() {
+    document.body.setAttribute("data-eager-readable-state", "loading");
   }
 
   if (document.readyState === "loading") {
-    // TODO fix bootstrap setup
     window.addEventListener("load", updateElements);
+    document.addEventListener("DOMContentLoaded", bootstrap);
   } else {
+    bootstrap();
     updateElements();
   }
 
   window.INSTALL_SCOPE = {
     setOptions: function setOptions(nextOptions) {
-      clearTimeout(updateTimeout);
       options = nextOptions;
-      // TODO set a timeout that blurs the screen and doesn't reload until x seconds
-      updateTimeout = setTimeout(resetLineHeight, UPDATE_DELAY);
+
+      previousElements.forEach(function (_ref) {
+        var element = _ref.element;
+        var lineHeight = _ref.lineHeight;
+        return element.style.lineHeight = lineHeight;
+      });
+      previousElements = [];
+
+      updateElements();
     }
   };
 })();
