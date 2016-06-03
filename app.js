1280b28bc5ef0655660dd7e6380f2011321fea00
Teffen Ellis (Eric) <teffen@nirri.us>
2016-06-03 15:24:33 -0700

    Move preview toggle to install.json

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 (function () {
   if (!window.addEventListener) return // Check for IE9+
 
+  let options = INSTALL_OPTIONS
+
   const PHI = (1 + Math.sqrt(5)) / 2 // 1.61803398874989 or "The Golden Ratio"
   const PHI_PRIME = 1 / (2 * PHI)
   const SELECTORS = "address, blockquote, footer, h1, h2, h3, h4, h5, h6, header, li, q, p"
 
-  const styleToggle = document.createElement("eager-style-toggle")
   const loadingStyle = document.createElement("style")
-
-  styleToggle.title = "Toggle preview"
 
   loadingStyle.innerHTML = `
     body, body * {
@@ -24,18 +23,17 @@
     return Math.round(result * fontSize) / fontSize // Lower precision.
   }
 
-  function updateToggleState() {
-    styleToggle.setAttribute("data-state", previousElements.length ? "enabled" : "disabled")
-  }
-
   function reset() {
     previousElements.forEach(({element, lineHeight}) => element.style.lineHeight = lineHeight)
     previousElements = []
-
-    updateToggleState()
   }
 
   function updateElements() {
+    if (INSTALL_ID === "preview" && !options.showPreview) {
+      reset()
+      return
+    }
+
     const elements = Array.prototype.slice.call(document.querySelectorAll(SELECTORS))
 
     elements.forEach(element => {
@@ -48,32 +46,25 @@
       element.style.lineHeight = calcBaseLineHeight(fontSize, element.clientWidth)
     })
 
-    if (INSTALL_ID === "preview") updateToggleState()
 
     loadingStyle.parentNode && loadingStyle.parentNode.removeChild(loadingStyle)
-  }
-
-  function bootstrap() {
-    if (INSTALL_ID === "preview") {
-      styleToggle.addEventListener("click", () => {
-        if (previousElements.length) reset()
-        else updateElements()
-      })
-
-      updateToggleState()
-
-      document.body.appendChild(styleToggle)
-    }
-
-    updateElements()
   }
 
   document.head.appendChild(loadingStyle)
 
   if (document.readyState === "loading") {
-    window.addEventListener("load", bootstrap)
+    window.addEventListener("load", updateElements)
   }
   else {
-    bootstrap()
+    updateElements()
+  }
+
+  window.INSTALL_SCOPE = {
+    setOptions(nextOptions) {
+      options = nextOptions
+
+      reset()
+      updateElements()
+    }
   }
 }())
--- build/app.js
+++ build/app.js
@@ -3,14 +3,13 @@
 (function () {
   if (!window.addEventListener) return; // Check for IE9+
 
+  var options = INSTALL_OPTIONS;
+
   var PHI = (1 + Math.sqrt(5)) / 2; // 1.61803398874989 or "The Golden Ratio"
   var PHI_PRIME = 1 / (2 * PHI);
   var SELECTORS = "address, blockquote, footer, h1, h2, h3, h4, h5, h6, header, li, q, p";
 
-  var styleToggle = document.createElement("eager-style-toggle");
   var loadingStyle = document.createElement("style");
-
-  styleToggle.title = "Toggle preview";
 
   loadingStyle.innerHTML = "\n    body, body * {\n      color: transparent !important;\n    }\n  ";
 
@@ -22,10 +21,6 @@
     return Math.round(result * fontSize) / fontSize; // Lower precision.
   }
 
-  function updateToggleState() {
-    styleToggle.setAttribute("data-state", previousElements.length ? "enabled" : "disabled");
-  }
-
   function reset() {
     previousElements.forEach(function (_ref) {
       var element = _ref.element;
@@ -33,11 +28,14 @@
       return element.style.lineHeight = lineHeight;
     });
     previousElements = [];
-
-    updateToggleState();
   }
 
   function updateElements() {
+    if (INSTALL_ID === "preview" && !options.showPreview) {
+      reset();
+      return;
+    }
+
     var elements = Array.prototype.slice.call(document.querySelectorAll(SELECTORS));
 
     elements.forEach(function (element) {
@@ -50,30 +48,23 @@
       element.style.lineHeight = calcBaseLineHeight(fontSize, element.clientWidth);
     });
 
-    if (INSTALL_ID === "preview") updateToggleState();
-
     loadingStyle.parentNode && loadingStyle.parentNode.removeChild(loadingStyle);
-  }
-
-  function bootstrap() {
-    if (INSTALL_ID === "preview") {
-      styleToggle.addEventListener("click", function () {
-        if (previousElements.length) reset();else updateElements();
-      });
-
-      updateToggleState();
-
-      document.body.appendChild(styleToggle);
-    }
-
-    updateElements();
   }
 
   document.head.appendChild(loadingStyle);
 
   if (document.readyState === "loading") {
-    window.addEventListener("load", bootstrap);
+    window.addEventListener("load", updateElements);
   } else {
-    bootstrap();
+    updateElements();
   }
+
+  window.INSTALL_SCOPE = {
+    setOptions: function setOptions(nextOptions) {
+      options = nextOptions;
+
+      reset();
+      updateElements();
+    }
+  };
 })();
